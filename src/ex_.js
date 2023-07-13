87120a5c14009000d4ed201fa3b672d7c2ec73ae
Gustavo gomes <coven688@exemplo.com>
2023-07-13 14:10:06 -0300

    firts commit

---
+++ src/ex_.js
@@ -0,0 +1,9 @@
+"use strict";
+function mult(n1, n2) {
+    return n1 * n2;
+}
+console.log(mult(5, 5));
+function salutation(nome) {
+    return nome;
+}
+console.log(`Ola ${salutation('Gustavo')}`);
---
+++ src/ex_.ts
@@ -0,0 +1,11 @@
+function mult (n1: number, n2:number): number {
+    return n1 * n2
+}
+
+console.log(mult(5, 5))
+
+function salutation(nome: string):string {
+    return nome
+}
+
+console.log(`Ola ${salutation('Gustavo')}`)
